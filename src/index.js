5b8615e8376a8e3df904026fa16c41e7a964be68
PRODY (Juanma) <69425794+Juanma-venom@users.noreply.github.com>
2025-07-01 13:40:20 +0000

    Contact List

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { HashRouter } from "react-router-dom";
+import Layout from "./Layout/Layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
-    <App />
+    <Layout />
   </HashRouter>
 );
---
+++ src/views/home.jsx
@@ -0,0 +1,12 @@
+// src/views/Home.jsx
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-center mt-5">
+      <h1>¡Hola! Esta es tu lista de contactos</h1>
+    </div>
+  );
+};
+
+export default Home;
